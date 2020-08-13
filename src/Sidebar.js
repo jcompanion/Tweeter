853780db270abf6135acefd82c5f2962b072921b
jcompanion <31940636+jcompanion@users.noreply.github.com>
2020-08-13 10:02:14 -0500

    Initial commit

---
+++ src/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Sidebar.css";
+import TwitterIcon from '@material-ui/icons/Twitter';
+
+function Sidebar() {
+    return (
+        <div class="sidebar">
+            <TwitterIcon />
+            
+        </div>
+    )
+}
+
+export default Sidebar;
